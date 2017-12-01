79c9d0e7891c062e9a176143e8fb007b6687cf4c
adrianh589 <33093678+adrianh589@users.noreply.github.com>
2017-12-01 16:11:02 -0500

    Update main.cpp

--- MatrizParImpNegPos/main.cpp
+++ MatrizParImpNegPos/main.cpp
@@ -15,7 +15,7 @@ int main()
     int even_numbers_counter=0;
     int odd_numbers_counter=0;
 
-	cout<< "Defina el tamaño de la matriz"<<endl;
+	cout<< "Defina el tamaÃ±o de la matriz"<<endl;
     cin>>n;//Read the length of the matrix
     int numbers[n][n];
     cout<<"Introduzca los numeros: ";
@@ -31,27 +31,28 @@ int main()
 
     cout<<"La matriz original es"<<endl;
 
+    //Print the original matrix
     for(i =0; i<n;i++)
     {
             	for(j=0; j<n;j++)
             	{
-            		cout<<numbers[j][i]<<" ";//Print the original matrix
+            		cout<<numbers[j][i]<<" ";
             	}
                    cout<<""<<endl;
     }
 
 
-
+     //Add odd, even, positive and negative to respective counters
      for ( i=0; i<n;i++) {
 			for( j=0;j<n;j++) {
-				if (numbers[i][j]>=0) {
+				if (numbers[i][j]>=0) {//positive numbers
 					positive_numbers_counter +=1;
 				}else {
-					negative_numbers_counter+=1;
+					negative_numbers_counter+=1;//negative numbers
 				}if(numbers[i][j]%2==0) {
-					even_numbers_counter+=1;
+					even_numbers_counter+=1;//even numbers
 				}else {
-					odd_numbers_counter+=1;
+					odd_numbers_counter+=1;//odd numbers
 				}
 			}
 		}
@@ -61,6 +62,7 @@ int main()
 
 
 		cout<<"Hay "<<positive_numbers_counter<<" numeros positivos y son: "<<endl;
+		//Show in the console, the positive numbers
 		for ( i=0; i<n;i++) {
 			for( j=0;j<n;j++) {
 				if (numbers[i][j]>=0) {
@@ -70,7 +72,8 @@ int main()
 		}
 
 		cout<<""<<endl;
-
+		
+		//Print negative numbers
 		cout<<"\nHay "<<negative_numbers_counter<<" numeros negativos y son: "<<endl;
 		for ( i=0; i<n;i++) {
 			for( j=0;j<n;j++) {
@@ -82,6 +85,7 @@ int main()
 
 		cout<<""<<endl;
 
+		//Print even numbers
 		cout<<"\nHay "<<even_numbers_counter<<" numeros pares y son: "<<endl;
 		for ( i=0; i<n;i++) {
 			for( j=0;j<n;j++) {
@@ -93,6 +97,7 @@ int main()
 
 		cout<<""<<endl;
 
+		//Print odd numbers
 		cout<<"\nHay "<<odd_numbers_counter<<" numeros impares y son: "<<endl;
 		for ( i=0; i<n;i++) {
 			for( j=0;j<n;j++) {
