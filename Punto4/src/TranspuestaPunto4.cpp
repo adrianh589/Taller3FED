2c6502b55013b52ac3ed57de64cfb5ed79b6cf01
adrianh589 <33093678+adrianh589@users.noreply.github.com>
2017-12-05 13:14:38 -0500

    Update TranspuestaPunto4.cpp

--- Punto4/src/TranspuestaPunto4.cpp
+++ Punto4/src/TranspuestaPunto4.cpp
@@ -11,22 +11,24 @@ using namespace std;
 int main()
 
 {
-	int i;//Variable to columns of the matrix
-	int j;//Variable to rows of the matrix
-        int n;//Variable to define the matrix
-	int numero;
-	cout<< "Defina el tamaño de la matriz"<<endl;
+    int i;//Variable to columns of the matrix
+    int j;//Variable to rows of the matrix
+    int n;//Variable to define the matrix
+    int number;//Variable to read numbers
+    cout<< "Defina el tamaño de la matriz"<<endl;
+
     cin>>n;//Read the length of the matrix
     int matriz[n][n];
-    cout<<"Introduzca los numeros: ";
+    cout<<"Introduzca los numeros "<<n*n<<" numeros";
 
     for(i =0; i<n;i++)
     {
-    	for(j=0; j<n;j++)
-    	{
-    		cin>>numero;//Read the numbers in the console.
-    		matriz[i][j]=numero;//Add to the matrix the numbers
-    	}
+        for(j=0; j<n;j++)
+        {
+            cin>>number;//Read the numbers in the console.
+            matriz[i][j]=number;//Add to the matrix the numbers
+            cin.ignore(1);//Ignore the commas
+        }
 
     }
 
@@ -35,11 +37,11 @@ int main()
     //Print the original matrix
     for(i =0; i<n;i++)
     {
-            	for(j=0; j<n;j++)
-            	{
-            		cout<<matriz[i][j]<<" ";
-            	}
-                   cout<<""<<endl;
+            for(j=0; j<n;j++)
+            {
+                cout<<matriz[i][j]<<" ";
+            }
+               cout<<""<<endl;
     }
 
     cout<<"La transpuesta de esta matriz es"<<endl;
@@ -47,17 +49,13 @@ int main()
     //Print the transpose of the matrix
     for(i =0; i<n;i++)
     {
-        	for(j=0; j<n;j++)
-        	{
-        		cout<<matriz[j][i]<<" ";
-        	}
+            for(j=0; j<n;j++)
+            {
+                cout<<matriz[j][i]<<" ";
+            }
                  cout<<""<<endl;
      }
 
-	return 0;
+    return 0;
 
-}
-
-
-
-
+    }
